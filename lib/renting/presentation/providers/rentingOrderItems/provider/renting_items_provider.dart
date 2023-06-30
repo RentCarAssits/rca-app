03c57b71598cd39472f96963020cb3bdf803ab5a
Apolosmllb <minermllb@gmail.com>
2023-06-30 03:49:24 -0500

    fix: Type 'any' not found.

--- lib/renting/presentation/providers/rentingOrderItems/provider/renting_items_provider.dart
+++ lib/renting/presentation/providers/rentingOrderItems/provider/renting_items_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:rca_app/renting/domain/domain.dart';
 import 'package:rca_app/renting/domain/entities/rentingOrderItem.dart';
 import 'package:rca_app/renting/domain/repositories/rentingOrderItem_repository.dart';
 import 'package:rca_app/renting/presentation/providers/rentingOrderItems/provider/renting_items_repository_provider.dart';
