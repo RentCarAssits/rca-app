2c98cababf42e4732fe3d7a2d15dafbe3cfcc535
Miner Lozano <54141767+Apolosmllb@users.noreply.github.com>
2023-11-23 12:19:02 -0500

    Update vehicles_datasource_impl.dart

--- lib/renting/infrastructure/datasources/vehicles_datasource_impl.dart
+++ lib/renting/infrastructure/datasources/vehicles_datasource_impl.dart
@@ -9,7 +9,7 @@ class VehiclesDatasourceImpl extends VehiclesDatasource {
 
   VehiclesDatasourceImpl({required this.accessToken})
       : dio = Dio(BaseOptions(
-            baseUrl: ('https://rca.azurewebsites.net/api/v1'),
+            baseUrl: ('https://rca-iot-parsimonia.azurewebsites.net/api/v1'),
             headers: {'Authorization': 'Bearer $accessToken'}));
 
   ///Vehicles
